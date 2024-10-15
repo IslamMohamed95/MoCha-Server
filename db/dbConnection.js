85a28aa9834b8c9aa110a32b2ac12fe460a607de
islam <islammooma95@gmail.com>
2024-10-15 17:43:51 +0400

    ..

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://islam:P367uuYlQJr52iiv@mocha.lgqcneu.mongodb.net/mocha?retryWrites=true&w=majority",
+  "mongodb+srv://islam:FvLwH4oG63BBzzeM@mocha.lgqcneu.mongodb.net/",
   () => {
     console.log("Database is connected");
   }
 );
 
 //mongodb+srv://mochauser:1234@mocha.msg5s.mongodb.net/mocha?retryWrites=true&w=majority
+//mongodb+srv://islam:<db_password>@mocha.lgqcneu.mongodb.net/
+//mongodb+srv://islam:P367uuYlQJr52iiv@mocha.lgqcneu.mongodb.net/mocha?retryWrites=true&w=majority
