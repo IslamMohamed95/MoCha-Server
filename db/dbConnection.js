2f62853a11a5d0b223714e84c8b6077d1593c791
islam <islammooma95@gmail.com>
2024-10-15 17:46:53 +0400

    ..

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://islam:FvLwH4oG63BBzzeM@mocha.lgqcneu.mongodb.net/",
+  "mongodb+srv://islam:FvLwH4oG63BBzzeM@mocha.lgqcneu.mongodb.net/mocha?retryWrites=true&w=majority",
   () => {
     console.log("Database is connected");
   }
@@ -9,4 +9,4 @@ mongoose.connect(
 
 //mongodb+srv://mochauser:1234@mocha.msg5s.mongodb.net/mocha?retryWrites=true&w=majority
 //mongodb+srv://islam:<db_password>@mocha.lgqcneu.mongodb.net/
-//mongodb+srv://islam:P367uuYlQJr52iiv@mocha.lgqcneu.mongodb.net/mocha?retryWrites=true&w=majority
+//mongodb+srv://islam:FvLwH4oG63BBzzeM@mocha.lgqcneu.mongodb.net/mocha?retryWrites=true&w=majority
