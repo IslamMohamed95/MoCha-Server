5abe42a45c46c46c52c53f7cc2704da3e8acad70
islam <islammooma95@gmail.com>
2025-04-06 11:54:23 +0200

    ..

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 mongoose.connect(
-  "mongodb+srv://islam:FvLwH4oG63BBzzeM@mocha.lgqcneu.mongodb.net/mocha?retryWrites=true&w=majority",
+  "mongodb+srv://islam:mocha123@mocha.lgqcneu.mongodb.net/",
   () => {
     console.log("Database is connected");
   }
 );
 
 //mongodb+srv://mochauser:1234@mocha.msg5s.mongodb.net/mocha?retryWrites=true&w=majority
-//mongodb+srv://islam:<db_password>@mocha.lgqcneu.mongodb.net/
+//mongodb+srv://islam:FvLwH4oG63BBzzeM@mocha.lgqcneu.mongodb.net/
 //mongodb+srv://islam:FvLwH4oG63BBzzeM@mocha.lgqcneu.mongodb.net/mocha?retryWrites=true&w=majority
